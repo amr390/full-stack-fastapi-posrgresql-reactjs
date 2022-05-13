66c80653343a8f7376535a465c3bfa801a82466a
amr <anma@manning.com>
2022-05-13 07:59:33 +0200

    playing around to get a taste of tailwindcss

--- frontendr/tailwind.config.js
+++ frontendr/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "src/pages/*.{ts,tsx,html}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/bkn-image.jpg')",
+      }
+    },
   },
   plugins: [],
 }
