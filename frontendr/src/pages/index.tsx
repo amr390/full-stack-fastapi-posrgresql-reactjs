c5c2ba9838dd942bb7b0d5591aa0aec31624b19e
amr <dquijote22@gmail.com>
2022-05-13 19:16:13 +0200

    playing with tailwindcss, trying implementing a simple figma design

--- frontendr/src/pages/index.tsx
+++ frontendr/src/pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div
-      className='bg-cover bg-no-repeat bg-center'
+      className='h-full bg-no-repeat bg-center bg-gradient-to-b from-green-500 to-green-50 bg-cover '
       style={{
         backgroundImage: "url('/img/bkn-image.jpg')",
       }}
@@ -17,16 +17,21 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className='min-h-screen px-4 flex flex-1 flex-col justify-center items-center'>
-        <section className="h-1/4">
-          <div className='h-full'>
-            <Image src='/img/Logo.svg' alt='Logo' width={298} height={64} layout='responsive' />
+      <main className='h-full px-4 flex flex-1 flex-col justify-center items-center'>
+        <section className="flex flex-col justify-center items-center h-1/4 w-full ">
+          <div className='flex flex-col justify-center items-center w-full'>
+            <Image src='/img/Logo.svg' alt='Logo' width={128} height={128} layout='fixed' />
           </div>
         </section>
-
-        <div className='h-fit'>
-          <span> here it goes the content </span>
-        </div>
+        <section className='flex flex-col justify-end items-end w-full h-3/4 lg:h-[600px] lg:justify-center lg:items-center'>
+          <div className='flex flex-col justify-center items-center rounded-t-lg lg:rounded-b-lg rounded-tr-lg w-full lg:w-1/4 lg:justify-center lg:align-middle h-3/4 bg-sky-500/80'>
+            <form className='flex flex-col py-4' action="some-action" method="post">
+              <input className="px-4 bg-sky-500/10 border-b-[0.1rem] border-white outline-none placeholder:text-sky-50 focus:border-b-2 text-sky-50" type="text" id="first" name="first" placeholder="Email"/>
+              <input className="px-4 bg-sky-500/10 border-b-[0.1rem] border-white outline-none placeholder:text-sky-50 focus:border-b-2 text-sky-50 pt-8" type="password" id="last" name="last" placeholder="Password" />
+              <button className="bg-sky-50 rounded-md mt-16 h-10 text-xl" type="submit" >Submit</button>
+            </form>
+          </div>
+        </section>
       </main>
 
       {/* <footer className={styles.footer}> */}
