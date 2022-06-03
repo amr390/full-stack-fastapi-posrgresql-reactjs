4cb9f9566c470f63ddf3a68c3c3ed3628aa4aa12
amr <dquijote22@gmail.com>
2022-06-03 19:21:22 +0200

    ...

--- frontendr/src/pages/index.tsx
+++ frontendr/src/pages/index.tsx
@@ -1,26 +1,7 @@
-import { Layout } from '../components/landing/_layout'
-import { Hero } from '../components/landing/hero/Hero'
-import { Services } from '../components/landing/services/Services'
-import { Featured } from '../components/landing/featured/Featured'
-import { Team } from '../components/landing/team/Team'
-import { Finisher } from '../components/landing/finisher/Finisher'
-import { Contact } from '../components/landing/contact/Contact'
+import React from 'react'
 
-const IndexPage = () => (
-  <Layout title='Home | Next.js + TypeScript Example'>
-    <Hero />
-    <Services />
-    <Featured />
-    <Team />
-    <Finisher />
-    <Contact />
-  </Layout>
-)
+const IndexPage = () => {
+  return <div>index</div>
+}
 
 export default IndexPage
-
-
-
-
-
-
